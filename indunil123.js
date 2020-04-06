8aedc51716c72d2ac6f534e2d21cf5fec4e0f9d2
Indunil1992 <indunil@adroitlogic.com>
2020-04-06 17:12:31 +0530

    12

--- indunil123.js
+++ indunil123.js
@@ -1,4 +1,21 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let data = await sns.subscribe({
+            TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
+            Protocol: "http",
+            Endpoint: "https://052p9kjn18.execute-api.us-east-1.amazonaws.com/TestM/test/indusns	"
+        }).promise();
+        console.log("sucsess")
+        console.log(data)
+
+    } catch (err) {
+                console.log("errrrrr")
+        console.log(err)
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
 };
