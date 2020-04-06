1ffe1856584d9b5fe6a04d8440271bdeee101b63
Indunil1992 <indunil@adroitlogic.com>
2020-04-06 17:19:04 +0530

    1q

--- indunil123.js
+++ indunil123.js
@@ -5,14 +5,14 @@ exports.handler = async (event) => {
     try {
         let data = await sns.subscribe({
             TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
-            Protocol: "http",
+            Protocol: "https",
             Endpoint: "https://052p9kjn18.execute-api.us-east-1.amazonaws.com/TestM/test/indusns	"
         }).promise();
         console.log("sucsess")
         console.log(data)
 
     } catch (err) {
-                console.log("errrrrr")
+        console.log("errrrrr")
         console.log(err)
         // error handling goes here
     };
